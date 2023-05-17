6fda20863bd1b866b0163350a1c6ac1dd71cb5c3
jakeahend <111516293+jakeahend@users.noreply.github.com>
2023-05-17 02:47:16 -0500

    animation tuning

--- src/components/Mode.vue
+++ src/components/Mode.vue
@@ -7,16 +7,16 @@
 <template>
 	<div id="mode" @click="darkMode = !darkMode">
 		<Transition>
-			<img id="sun" v-if="!darkMode"/>
+			<img id="sun" v-show="!darkMode" />
+		</Transition>
+		<Transition :css="false">
+			<img id="sun-bg" v-show="!darkMode" />
 		</Transition>
 		<Transition>
-			<img id="sun-bg" v-if="!darkMode"/>
+			<img id="moon" v-show="darkMode" />
 		</Transition>
-		<Transition>
-			<img id="moon" v-if="darkMode"/>
-		</Transition>
-		<Transition>
-			<img id="moon-bg" v-if="darkMode"/>
+		<Transition :css="false">
+			<img id="moon-bg" v-show="darkMode" />
 		</Transition>
 	</div>
 </template>
@@ -25,8 +25,8 @@
 #mode {
 	cursor: pointer;
 	position: fixed;
-	width: 36px;
-	height: 36px;
+	width: 30px;
+	height: 30px;
 	top: 20px;
 	right: 20px;
 }
@@ -35,22 +35,23 @@
 	position: absolute;
 	top: 0px;
 	right: 0px;
-	width: 36px;
-	height: 36px;
-}
-
-#sun {
-	content: url('../assets/sun_ico.png');
+	width: 30px;
+	height: 30px;
 }
 
 #sun-bg, #moon-bg {
 	position: absolute;
 	top: -8px;
 	right: -8px;
-	width: 52px;
-	height: 52px;
+	width: 46px;
+	height: 46px;
 	z-index: -1;
-	animation: spin 10s linear infinite;
+	animation: spin 25s linear infinite;
+	opacity: 0.75;
+}
+
+#sun {
+	content: url('../assets/sun_ico.png');
 }
 
 #sun-bg {
@@ -66,6 +67,12 @@
 }
 
 @keyframes spin {
+	0% {
+		opacity: 0;
+	}
+	5% {
+		opacity: 1;
+	}
 	25% { 
 		transform: rotate(90deg);
 		filter: brightness(1.2);
@@ -78,19 +85,23 @@
 		transform: rotate(270deg);
 		filter: brightness(1.45);
 	}
+	95% {
+		opacity: 1;
+	}
 	100% { 
 		transform: rotate(360deg);
-		filter: brightness(1);
+		filter: brightness(1.8);
+		opacity: 0;
 	}
 }
 
 .v-enter-active,
 .v-leave-active {
-  transition: opacity 0.75s ease;
+  transition: opacity 1s ease;
 }
 
 .v-enter-from,
 .v-leave-to {
-  opacity: 0;
+	opacity: 0;
 }
 </style>
