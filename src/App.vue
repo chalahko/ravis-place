48bcc7d9f9a873f718cda8248abc6a696e59bd5e
jakeahend <111516293+jakeahend@users.noreply.github.com>
2023-05-17 00:03:12 -0500

    added new stuff

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,104 @@
 <script setup lang="ts">
+  import Mode from './components/Mode.vue'
+  import { onMounted } from 'vue';
 
+  onMounted(() => {
+    window.addEventListener('scroll', debounce(scrollUpdate, 250))
+  })
+
+  function scrollUpdate() {
+    const timeline = document.getElementById('timeline')
+
+    const scroll = document.documentElement.scrollTop
+
+    if(timeline) {
+      timeline.style.height = `calc(${scroll}px + 85vh)`
+      console.log('setting height to ' + scroll)
+    }
+  }
+
+  function debounce (method: Function, wait: number) {
+    let timeout: number
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout)
+      }
+      timeout = setTimeout(method, wait)
+    }
+  }
 </script>
 
 <template>
-  <h1 id="ravi">Ravi Chalahko</h1>
-  <svg width="100" height="600">
-    <defs>
-      <linearGradient id="grad" gradientUnits="userSpaceOnUse" x1="50" x2="50" y1="50" y2="550">
-        <stop offset="5%" stop-color="#547E96" />
-        <stop offset="95%" stop-color="#373052" />
-      </linearGradient>
-    </defs>
-
-    <path id="path" d="M 50 50 v 500" />
-  </svg>
+  <Mode></Mode>
+  <section style="background-color: #666699;">
+    <p>
+      shroud: campfire<br>
+      torches: stars<br>
+      carteneau: glyph
+    </p>
+  </section>
+  <section style="background-color: #52527a;"></section>
+  <section style="background-color: #3d3d5c;"></section>
+  <div id="timeline"></div>
+  <div id="dot"></div>
 </template>
 
 <style scoped>
-#path {
-  stroke: url(#grad);
+#dot {
+  position: absolute;
+  top: 50px;
+  right: 22vw;
+  transform: translateX(50%) translateY(-50%);
+  height: 10px;
+  width: 10px;
+  background-color: #e1e1e1;
+  border-radius: 10px;
+  border: 4px solid rgb(74, 57, 100);
+  z-index: 15;
+}
+
+section {
+  display: block;
+  box-sizing: border-box;
+  height: 100vh;
+  width: 60vw;
+  scroll-snap-align: start;
+  padding: 20px;
+}
+
+#timeline {
+  position: absolute;
+  transform: translateX(50%);
+  top: 50px;
+  right: 22vw;
+  width: 6px;
+  height: 85vh;
+  background-color: rgb(74, 57, 100);
+  z-index: 10;
+  transition: height .5s;
+  transition-delay: .2s;
+  border-radius: 6px;
+}
+
+path {
+  stroke: #b33e36;
+  stroke-linecap: round;
   stroke-width: 3px;
+  fill: none;
 }
 
 #ravi {
-  background-image: linear-gradient(180deg, #547E96 0%, #373052 100%);
+  background-image: linear-gradient(0deg, #b33e36 0%, #BF9630 100%);
   -webkit-background-clip: text;
   background-clip: text;
   -webkit-text-fill-color: transparent;
-  -webkit-text-stroke: 0.01em rgba(143,143,143,0.541);
+  -webkit-text-stroke: 0.01em rgba(33,33,33,0.541);
   font-weight: 400;
+}
+
+circle {
+  fill: #d9d9d9;
+  stroke: #b33e36;
+  stroke-width: 3px;
 }
 </style>
---
+++ src/components/Mode.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+	import { ref } from 'vue';
+
+	let darkMode = ref(true)
+</script>
+
+<template>
+	<div id="mode" @click="darkMode = !darkMode">
+		<Transition>
+			<img id="sun" v-if="!darkMode"/>
+		</Transition>
+		<Transition>
+			<img id="sun-bg" v-if="!darkMode"/>
+		</Transition>
+		<Transition>
+			<img id="moon" v-if="darkMode"/>
+		</Transition>
+		<Transition>
+			<img id="moon-bg" v-if="darkMode"/>
+		</Transition>
+	</div>
+</template>
+
+<style scoped>
+#mode {
+	cursor: pointer;
+	position: fixed;
+	width: 36px;
+	height: 36px;
+	top: 20px;
+	right: 20px;
+}
+
+#sun, #moon {
+	position: absolute;
+	top: 0px;
+	right: 0px;
+	width: 36px;
+	height: 36px;
+}
+
+#sun {
+	content: url('../assets/sun_ico.png');
+}
+
+#sun-bg, #moon-bg {
+	position: absolute;
+	top: -8px;
+	right: -8px;
+	width: 52px;
+	height: 52px;
+	z-index: -1;
+	animation: spin 10s linear infinite;
+}
+
+#sun-bg {
+	content: url('../assets/sun_flare.png');
+}
+
+#moon {
+	content: url('../assets/moon_ico.png');
+}
+
+#moon-bg {
+	content: url('../assets/moon_flare.png');
+}
+
+@keyframes spin {
+	25% { 
+		transform: rotate(90deg);
+		filter: brightness(1.2);
+	}
+	50% { 
+		transform: rotate(180deg);
+		filter: brightness(0.7);
+	}
+	75% { 
+		transform: rotate(270deg);
+		filter: brightness(1.45);
+	}
+	100% { 
+		transform: rotate(360deg);
+		filter: brightness(1);
+	}
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.75s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
